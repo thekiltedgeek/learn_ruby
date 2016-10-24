aacc8fc28fac078800edd58884ec1649eed3242a
Larry Shanahan <thekiltedgeek@gmail.com>
2016-10-24 19:33:59 -0400

    Completed basic exercise plus optional multiply exercise

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
 #write your code here
+def add(x,y)
+    return x + y
+end
+
+def subtract(x,y)
+    return x - y
+end
+
+def sum(array)
+    result = 0
+    if array.length > 0
+        array.each do |item|
+            result += item
+        end
+    end
+    return result
+end
+
+def multiply(array)
+    result = 1
+    #if array.length > 1
+        array.each do |item|
+            result = result * item
+        end
+    #end
+    return result
+end
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,14 +79,18 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
-
-  it "multiplies several numbers"
-  
+  it "multiplies two numbers" do
+    expect(multiply([3,4])).to eq(12)
+  end
+    it "multiplies several numbers" do
+    expect(multiply([1,2,3])).to eq(6)
+    end
 end
 
 describe "#power" do
-  it "raises one number to the power of another number"
+  it "raises one number to the power of another number" do
+      expect(power(2,3)).to eq(8)
+  end
 end
 
 # http://en.wikipedia.org/wiki/Factorial
