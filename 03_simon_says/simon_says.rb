cd725b6cf9373602f25ec358d5b09e049d3284d9
Larry Shanahan <thekiltedgeek@gmail.com>
2016-10-24 23:24:47 -0400

    Completed Simon Says exercises

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
 #write your code here
+def echo(text)
+    return text
+end
+
+def shout(text)
+    return text.upcase
+end
+
+def repeat(text, times=2)
+    return ((text + " ") * times).chop
+end
+
+def start_of_word(text, len = 1)
+    return text[0..len-1]
+end
+
+def first_word(text)
+    words = text.split(' ')
+    return words[0]
+end
+
+def titleize(text)
+    words = text.split(" ")
+    words.each.with_index do |word, i|
+        if (i == 0 || i == words.length-1)
+            words[i] = word.capitalize
+        else
+            if word.length > 4
+                words[i] = word.capitalize
+            end
+        end
+    end
+    
+    return words.join(' ')
+end
