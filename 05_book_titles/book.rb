acdfc396a7ecda2eabe363a20d1992b292e87825
Larry Shanahan <thekiltedgeek@gmail.com>
2016-10-26 19:34:30 -0400

    Completed book_titles exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
 # write your code here
+    def title
+        @title
+    end
+    
+    def title=(t)
+        @title = capitalizer(t)
+    end
+    
+    private
+    
+    def capitalizer(text)
+        no_cap_words =["a","an","the","at","by","for","in","of","on","to","up","and","as","but","or","nor"]
+        words = text.split(' ')
+        words.each.with_index do |word,i|
+            if not (no_cap_words.include?(word))
+                words[i] = words[i].capitalize
+            end
+        end
+        words[0] = words[0].capitalize
+        return words.join(' ')
+    end
 end
+
+    
