d5c46dfa555bf64919201d1418696fa455c7c484
Larry Shanahan <thekiltedgeek@gmail.com>
2016-10-26 18:03:26 -0400

    Completed pig latin exercise

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,18 @@
 #write your code here
+def translate(phrase)
+    words = phrase.split(' ')
+    words.each.with_index do |word,i|
+        matches = /(^[^aeiou]?qu|[aeiou])/.match(word)
+        if matches != nil
+            if matches[1][-2..-1] == "qu"
+                pre = matches[1]
+            else
+                pre = matches.pre_match
+            end
+            words[i] = word.gsub(pre,'') + pre +"ay"
+        end
+    end
+    result = words.join(' ')
+    
+    return result
+end
