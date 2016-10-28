20ae4bfef00659d9dc93647fb64d64ddd5e8b037
Larry Shanahan <thekiltedgeek@gmail.com>
2016-10-28 15:59:31 -0400

    Completed exercise 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,15 @@
 class Timer
   #write your code here
+    attr_accessor :seconds
+    def initialize
+        @seconds = 0
+    end
+    
+    def time_string
+        hrs = @seconds / 3600
+        mins = (@seconds -= hrs*3600)/60
+        secs = @seconds -= mins*60
+        
+        return "#{hrs.to_s.rjust(2,'0')}:#{mins.to_s.rjust(2,'0')}:#{secs.to_s.rjust(2,'0')}"
+    end
 end
